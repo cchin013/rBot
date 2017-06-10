b244b7fe879a496ea714c7200bf9b1f749ee0161
Charlie C <cchin013@ucr.edu>
2017-06-10 13:15:16 -0700

    small switch case issue

--- rBot.h
+++ rBot.h
@@ -19,7 +19,7 @@ using namespace std;
 
 class rBot {
     public:
-    rBot(char const* nick, char const* pass);
+    rBot(char const* pass, char const* nick, char const* user);
     ~rBot();
     void start();
     bool find(char const* toLookThru, char const* toFind);
@@ -27,18 +27,20 @@ class rBot {
     private:
     int s; //socket info
     char const* port; //6667 for IRC bot
-    char const* nick; //username of the bot
     char const* pass; //OAuth password from http://twitchapps.com/tmi/
+    char const* nick; //username of the bot
+    char const* user;
     bool success; //Don't try to start the bot if the connection fails
     bool sendData(char const* msg); //Helper to commands; sends message or command in response to a command in chat
     void sendPong(char const* buf); //IRC chats send a PING and you need to respond with PONG or the bot disconnects
     void commands(char const* buf); //Deal with the proper response to commands from chat
 };
 
-rBot::rBot(char const* nick, char const* pass)
+rBot::rBot(char const* pass, char const* nick, char const* user)
 {
-    this->nick = nick;
     this->pass = pass;
+    this->nick = nick;
+    this->user = user;
 }
 
 rBot::~rBot()
@@ -57,14 +59,14 @@ void rBot::start()
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     int errCheck; //Store error messages if failed connection
-    if ((errCheck = getaddrinfo("irc.chat.twitch.tv", this->port, &hints, &servinfo)) != 0)
+    if ((errCheck = getaddrinfo("irc.chat.twitch.tv", port, &hints, &servinfo)) != 0)
     {
         this->success = false;
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(errCheck));
     }
     
     //Check if socket setup failed
-    if ((s = socket(servinfo->ai_family,servinfo->ai_socktype,servinfo->ai_protocol)) == -1) 
+    if ((s = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol))) 
     {
         perror("client: socket");
     }
@@ -73,23 +75,24 @@ void rBot::start()
     if (connect(s,servinfo->ai_addr, servinfo->ai_addrlen) == -1)
     {
         close(s);
-        perror("Client failed connection");
+        perror("Client successfully connected");
     }
     
-    //freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo);
     
     //Setup for receiving data
     int data; //Stores data
     char buf[MAXDATASIZE]; //Let's set the max data size(msg size) to 150
     
     int tick = 0; //amount of times data received
-    while (1)
+    while (1) //Main bot activity loop
     {
         ++tick;
         switch(tick) {
-            case 3:
-                sendData(nick);
+            case 1:
                 sendData(pass);
+                sendData(nick);
+                sendData(user);
                 break;
             case 4:
                 sendData("JOIN #noticemeskyoriwilldie\r\n"); //This line determines what channel you want the bot in
@@ -100,22 +103,17 @@ void rBot::start()
     
     //The following will deal with receiving commands from the chat
     data = recv(s,buf,MAXDATASIZE-1,0);
-    buf[data] = '\n';
+    buf[data] = '\0';
     cout << buf;
     commands(buf);
     
-    if (find(buf, "PING :tmi.twitch.tv"))
+    if (find(buf, "PING"))
     {
         sendPong(buf);
     }
     
     if (data == 0)
     {
-        cout << tick << endl;
-        cout << buf << endl;
-        cout << data << endl;
-        cout << this->nick << endl;
-        cout << this->pass << endl;
         cout << "Connection lost for some reason, figure it out" << endl;
         break;
     }
@@ -132,7 +130,7 @@ bool rBot::find(char const* toLookThru, char const* toFind)
     {
         bool found = true;
         for (unsigned j = 1; j < toFindLen; ++j) //This is very slow [O(n^2)]
-        {                                       //TODO: Optimize this I suppose?
+        {                                       //TODO: Optimize this I suppose? Can maybe speed it up through markov chains?
             if (toLookThru[j + i] != toFind[j]) //Matching the letters one by one for the words
             {                                   //i.e. if we need to find "BOB" it would compare
                 found = false;                  //the first letter of toLookThru to "B" then second letter to "O" etc
@@ -159,7 +157,7 @@ bool rBot::sendData(char const* msg)
 
 void rBot::sendPong(char const* buf)
 {
-    char const* toLookThru = "PING :tmi.twitch.tv";
+    char const* toLookThru = "PING ";
     
     for (unsigned i = 0; i < strlen(buf); ++i)
     {
@@ -182,27 +180,8 @@ void rBot::sendPong(char const* buf)
                     ++tick;
                 }
                 
-                char returnPong[tick + 13];
-                returnPong[0] = 'P';
-                returnPong[1] = 'O';
-                returnPong[2] = 'N';
-                returnPong[3] = 'G';
-                returnPong[4] = ' ';
-                returnPong[5] = ':';
-                returnPong[6] = 't';
-                returnPong[7] = 'm';
-                returnPong[8] = 'i';
-                returnPong[9] = '.';
-                returnPong[10] = 't';
-                returnPong[11] = 'w';
-                returnPong[12] = 'i';
-                returnPong[13] = 't';
-                returnPong[14] = 'c';
-                returnPong[15] = 'h';
-                returnPong[16] = '.';
-                returnPong[17] = 't';
-                returnPong[18] = 'v';
-                
+                char const* returnPong = "PONG ";
+
                 if (sendData(returnPong))
                 {
                     cout << "PING PONG" << endl;
@@ -217,7 +196,7 @@ void rBot::commands(char const* buf)
 {
     if (find(buf, "!elias"))
     {
-        sendData("PRIVMSG #noticemeskyoriwilldie: hello elias sells");
+        sendData("PRIVMSG #noticemeskyoriwilldie: hello elias sells\r\n");
     }
 }
 
